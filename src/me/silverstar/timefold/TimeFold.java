5182f2f99f15485149886a5727f506a08ab4cbce
Silverstar <admin@silversystem.de>
2011-05-19 19:42:01 +0200

    Version 0.4 - Fixed creating settings file, fixed crash on non-existent world, reworked the ActionListener to allow 0 value o day or night, general code cleanup.

--- src/me/silverstar/timefold/TimeFold.java
+++ src/me/silverstar/timefold/TimeFold.java
@@ -21,72 +21,93 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class TimeFold extends JavaPlugin {
 	public static final Logger log = Logger.getLogger("Minecraft");
 	public static Timer timer;
-	
+	public static boolean debug = false;
+
     public void onEnable() {
         //PluginManager pm = getServer().getPluginManager();
         //pm.registerEvent(Event.Type.BLOCK_BREAK, blockListener, Event.Priority.Normal, this);
         //pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Event.Priority.Normal, this);
     	int timertime = 5000;
-    	@SuppressWarnings("unused")
-		TimeFoldFileHandler FileHandler = new TimeFoldFileHandler(this);
+    	new TimeFoldFileHandler(this);
     	TimeFoldActionListener TimeFoldAction = new TimeFoldActionListener(this);
-    	
+
     	timer = new Timer(timertime, TimeFoldAction);
+    	timer.setInitialDelay(10000);
     	timer.start();
-    	
+
         PluginDescriptionFile pdfFile = this.getDescription();
         log.info("#" + pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
     }
     public void onDisable(){
     	timer.stop();
-    	
+
         PluginDescriptionFile pdfFile = this.getDescription();
         log.info("#" + pdfFile.getName() + " version " + pdfFile.getVersion() + " is disabled!");
     }
-    
+
     public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String args[]){
     	if(cmdLabel.equalsIgnoreCase("TimeFold")){
     		if(args.length == 0){
     			sender.sendMessage(getCycle(sender));    			
     		}else if(args.length == 1){
         		if(args[0].equalsIgnoreCase("get")) {
-            		String sendertoplayer[] = sender.toString().split("=");
-                	String player[] = sendertoplayer[1].split("}");
-            		Long time = getTime(Bukkit.getServer().getPlayer(player[0]));
+            		Long time = getTime(Bukkit.getServer().getPlayer(senderToPlayer(sender)));
             		sender.sendMessage(time.toString());
+        		}else if(args[0].equalsIgnoreCase("debug")){
+        			if(debug){
+        				debug = false;
+        				sender.sendMessage("Debug off");
+        			}else if(!debug){
+        				debug = true;
+        				sender.sendMessage("Debug on");
+        			}
         		}
     		}
     		return true;
     	}
     	return false;
     }
-    
+
     String senderToPlayer(CommandSender sender){
 		int start = sender.toString().indexOf("=") + 1;
 		int end = sender.toString().lastIndexOf("}");
 		return sender.toString().substring(start, end);
     }
-    
+
     long getTime(Player player) {
     	return player.getWorld().getTime();
     }
-    
+
     String getCycle(CommandSender sender){
     	int i = 0;
+		boolean found = false;
     	String world = Bukkit.getServer().getPlayer(senderToPlayer(sender)).getWorld().getName();
     	for(Map.Entry<String, String> entry : TimeFoldFileHandler.worlds.entrySet()){
-    		if(entry.getValue() == world){
+    		if(entry.getValue().equalsIgnoreCase(world)){
     			i = Integer.valueOf(entry.getKey());
+    			found = true;
     			break;
     		}
     	}
-    	
-    	if(TimeFoldActionListener.dayscomplete.get(i)){
-    		StringBuilder message = new StringBuilder().append(ChatColor.WHITE).append("It's night ").append(ChatColor.YELLOW).append(TimeFoldActionListener.nights.get(i)).append(ChatColor.WHITE).append(" of ").append(ChatColor.YELLOW).append(TimeFoldFileHandler.worlds.get(i+"2"));
-    		return message.toString();
+
+    	if(found){
+    		int nights = Integer.valueOf(TimeFoldFileHandler.worlds.get(String.valueOf(i)+"2"));
+    		int days = Integer.valueOf(TimeFoldFileHandler.worlds.get(String.valueOf(i)+"1"));
+    		if(nights == 0){
+    			return "It's day all the time.";
+    		}else if(days == 0){
+    			return "It's always night.";
+    		}else if(days == 1 && nights == 1){
+    			return "Normal day/night cycle. Look up in the sky!";
+    		}else if(TimeFoldActionListener.dayscomplete.get(i)){
+        		StringBuilder message = new StringBuilder().append(ChatColor.WHITE).append("It's night ").append(ChatColor.YELLOW).append(TimeFoldActionListener.nights.get(i)).append(ChatColor.WHITE).append(" of ").append(ChatColor.YELLOW).append(TimeFoldFileHandler.worlds.get(i+"2"));
+        		return message.toString();
+        	}else{
+        		StringBuilder message = new StringBuilder().append(ChatColor.WHITE).append("It's day ").append(ChatColor.YELLOW).append(TimeFoldActionListener.days.get(i)).append(ChatColor.WHITE).append(" of ").append(ChatColor.YELLOW).append(TimeFoldFileHandler.worlds.get(i+"1"));
+        		return message.toString();    		
+        	}
     	}else{
-    		StringBuilder message = new StringBuilder().append(ChatColor.WHITE).append("It's day ").append(ChatColor.YELLOW).append(TimeFoldActionListener.days.get(i)).append(ChatColor.WHITE).append(" of ").append(ChatColor.YELLOW).append(TimeFoldFileHandler.worlds.get(i+"1"));
-    		return message.toString();    		
+    		return "TimeFold is not configured for this world.";
     	}
     }
 }
--- src/me/silverstar/timefold/TimeFoldFileHandler.java
+++ src/me/silverstar/timefold/TimeFoldFileHandler.java
@@ -1,13 +1,19 @@
 package me.silverstar.timefold;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.bukkit.Bukkit;
 
 
 /**
@@ -21,30 +27,36 @@ public class TimeFoldFileHandler {
 	public TimeFoldFileHandler(TimeFold instance){
 		plugin = instance;
 		int Error = 0;
-		
+
 		if(!new File("plugins" + File.separator + "TimeFold").exists()){
 			try {
 				new File("plugins" + File.separator + "TimeFold").mkdir();
 			}catch(Exception e){
-				TimeFold.log.info("#TimeFold: Error: Can't create the TimeFold directory");
+				TimeFold.log.severe("#TimeFold: Can't create the TimeFold directory");
 				Error++;
 			}
 		}
-		
-		if(!new File("plugins" + File.separator + "TimeFold","TimeFold.settings").exists()){
+
+		if(!new File("plugins" + File.separator + "TimeFold" + File.separator + "TimeFold.settings").exists()){
 			try {
-				new File("plugins" + File.separator + "TimeFold","TimeFold.settings");	
+				new File("plugins" + File.separator + "TimeFold"+ File.separator + "TimeFold.settings");
+				String path = "plugins" + File.separator + "TimeFold" + File.separator + "TimeFold.settings";
+				FileOutputStream stream = new FileOutputStream(path);
+				DataOutputStream out = new DataOutputStream(stream);
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+				bw.write(Bukkit.getServer().getWorlds().get(0).getName().toString() + ":1:1");
+				bw.close();
 			}catch(Exception e){
-				TimeFold.log.info("#TimeFold: Error: Can't create the TimeFold settings file");
+				TimeFold.log.severe("#TimeFold: Can't create the TimeFold settings file");
 				Error++;
 			}
 		}
-		
+
 		if(Error == 0){
 			readconfig();
 		}
 	}
-	
+
 	private void readconfig(){
 		try {
 			String path = "plugins" + File.separator + "TimeFold" + File.separator + "TimeFold.settings";
@@ -55,18 +67,33 @@ public class TimeFoldFileHandler {
 			int i = 0;
 			while((strLine = br.readLine()) != null){
 				int k = 1;
-				String line[] = strLine.split(":");	
-				worlds.put(String.valueOf(i)+"0", line[0]);
-				while(k <= 2){
-					worlds.put(String.valueOf(i)+String.valueOf(k), line[k]);
-					k++;
+				String line[] = strLine.split(":");
+				if(checkWorld(line[0])){
+					worlds.put(String.valueOf(i)+"0", line[0]);
+					while(k <= 2){
+						worlds.put(String.valueOf(i)+String.valueOf(k), line[k]);
+						k++;
+					}
+					if(Integer.valueOf(worlds.get(String.valueOf(i)+"1")) == 0 && Integer.valueOf(worlds.get(String.valueOf(i)+"2")) == 0){
+		        		TimeFold.log.warning("#TimeFold: Misconfiguration for world \"" + line[0] + "\" found");
+		        		TimeFold.log.warning("#TimeFold: Setting days:nights to 1:1 for " + line[0]);
+		        		worlds.put((String.valueOf(i)+"1"), "1");
+		        		worlds.put((String.valueOf(i)+"2"), "1");
+					}
+					i++;
 				}
-				i++;
 			}
 		}catch (FileNotFoundException e){
-			TimeFold.log.info("#TimeFold: Error: Can't find the TimeFold settings file");
+			TimeFold.log.severe("#TimeFold: Can't find the TimeFold settings file");
 		}catch (Exception e){
-			TimeFold.log.info("#TimeFold: Error while reading the TimeFold settings file");
+			TimeFold.log.severe("#TimeFold: Error while reading the TimeFold settings file");
 		}
+	}
+
+	private boolean checkWorld(String world){
+		if(Bukkit.getServer().getWorld(world) != null){
+			return true;
+		}
+		return false;
 	}
 }
